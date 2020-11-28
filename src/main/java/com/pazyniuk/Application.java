252fad2051266dfe3048f589691f0443ab774075
damoklov <mishanya@protonmail.com>
2020-11-29 01:17:14 +0200

    Added launcher

---
+++ src/main/java/com/pazyniuk/Application.java
@@ -0,0 +1,12 @@
+package com.pazyniuk;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("com.pazyniuk")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
