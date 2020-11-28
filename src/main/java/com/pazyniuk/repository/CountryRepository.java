37875f78e1869bbb45c3e2985890ab7eda9dfbe0
damoklov <mishanya@protonmail.com>
2020-11-29 01:18:28 +0200

    Added repositories

---
+++ src/main/java/com/pazyniuk/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.pazyniuk.repository;
+
+import com.pazyniuk.domain.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/main/java/com/pazyniuk/repository/SongRepository.java
@@ -0,0 +1,9 @@
+package com.pazyniuk.repository;
+
+import com.pazyniuk.domain.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Long> {
+}
