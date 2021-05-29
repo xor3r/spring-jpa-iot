27b6a0195f9e5152e380fed36705351239d79f33
Mykhailo Pazyniuk <mishanya@protonmail.com>
2021-05-29 22:38:25 +0300

    Create HealthController.java

---
+++ src/main/java/com/pazyniuk/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.pazyniuk.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HealthController {
+  
+  @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+  public ResponseEntity returnHealthCheckResponse {
+      return new ResponseEntity<>("Everything is OK", HttpStatus.OK);
+  }
+}
